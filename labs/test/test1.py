eba14821e01a0d497e0ca5da297a50097b1ac189
Narusorn Ruengchot <narusorn.ru@ku.th>
2025-09-02 12:54:56 +0700

    .

---
+++ labs/test/test1.py
@@ -0,0 +1,8 @@
+student_name = "Alice"
+midterm = 85
+final = 92
+homework = 88
+
+total_grade = (midterm * 0.3) + (final * 0.4) + (homework * 0.3)
+
+print(f"{student_name} total grade = {total_grade:.2f}")
---
+++ labs/test/test2.py
@@ -0,0 +1,11 @@
+score = float(input("Please enter your score : "))
+if score >= 80:
+    print("very good")
+elif score >= 70:
+    print("good")
+elif score >= 60:
+    print("normal")
+elif score < 60:
+    print("should be improved")
+
+

415973e4eb5f41f5f1e3f623fe65f3465623ca8b
Narusorn Ruengchot <narusorn.ru@ku.th>
2025-09-23 14:52:59 +0700

    add lab week 9

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,21 @@
 import random
 
 def test_random():
+    # สร้างตัวแปร random_number ตัวแปรคือการสุ่มเลขระหว่าง 1 - 10
     random_number = random.randint(1, 100)
-    print(random_number)
-    
-test_random()
+    print("== Guess Number between (1 - 100) ==")
+    guess_number = input("What is your guess number: ")
+    guess_number = int(guess_number)
+
+    if random_number == guess_number:
+        print("Congratulations!")
+
+    elif random_number < guess_number:
+        print("Too high")
+
+    elif random_number > guess_number:
+        print("Too low")
+
+    #print(random_number)
+
+test_random() #เรียกใช้ฟังก์ชัน
